13438a9135bd7af91f393bddd7ef1efab58b4a07
Jan Breuer <j.m.br@gmx.de>
2024-12-09 14:56:56 +0100

    Plot MCT outlets for verification

--- src/test_cadet_core/MCT.py
+++ src/test_cadet_core/MCT.py
@@ -16,6 +16,9 @@ from cadet import Cadet
 from cadetrdm import ProjectRepo
 
 import bench_func as bf
+import utility.convergence as convergence
+import matplotlib.pyplot as plt
+import re
 
 # %% Run with CADET-RDM
 
@@ -24,6 +27,9 @@ def MCT_tests(n_jobs, database_path, small_test,
 
     os.makedirs(output_path, exist_ok=True)
     
+    convergence.std_plot_prep(benchmark_plot=False, linewidth=10,
+                              x_label='time / s', y_label='mol / $m^{-3}$')
+    
     Cadet.cadet_path = cadet_path
     
     model = bf.create_object_from_database(
@@ -31,8 +37,11 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_LRM_dynLin_1comp_MCTbenchmark.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
     model.save()
     
     model = bf.create_object_from_database(
@@ -40,8 +49,11 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_LRM_noBnd_1comp_MCTbenchmark.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
     model.save()
     
     model = bf.create_object_from_database(
@@ -49,8 +61,20 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_MCT1ch_noEx_noReac_benchmark1.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
+    
+    sol_name = model.filename
+    time = convergence.get_solution_times(sol_name)
+    channel1 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_000')
+
+    plt.plot(time, channel1, label='channel 1')
+    plt.legend(fontsize=20)
+    plt.savefig(re.sub(".h5", ".png", sol_name), dpi=100, bbox_inches='tight')
+    
     model.save()
     
     model = bf.create_object_from_database(
@@ -58,8 +82,20 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_MCT1ch_noEx_reac_benchmark1.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
+    
+    sol_name = model.filename
+    time = convergence.get_solution_times(sol_name)
+    channel1 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_000')
+
+    plt.plot(time, channel1, label='channel 1')
+    plt.legend(fontsize=20)
+    plt.savefig(re.sub(".h5", ".png", sol_name), dpi=100, bbox_inches='tight')
+    
     model.save()
     
     model = bf.create_object_from_database(
@@ -67,8 +103,22 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_MCT2ch_oneWayEx_reac_benchmark1.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
+    
+    sol_name = model.filename
+    time = convergence.get_solution_times(sol_name)
+    channel1 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_000')
+    channel2 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_001')
+
+    plt.plot(time, channel1, label='channel 1')
+    plt.plot(time, channel2, label='channel 2')
+    plt.legend(fontsize=20)
+    plt.savefig(re.sub(".h5", ".png", sol_name), dpi=100, bbox_inches='tight')
+    
     model.save()
     
     model = bf.create_object_from_database(
@@ -76,6 +126,23 @@ def MCT_tests(n_jobs, database_path, small_test,
         cadet_config_json_name='configuration_MCT3ch_twoWayExc_reac_benchmark1.json',
         output_path=str(output_path)
         )
-    model.run()
+    data = model.run()
     model.load()
+    if not data.return_code == 0:
+        print(data.error_message)
+        raise Exception(f"simulation failed")
+    
+    sol_name = model.filename
+    time = convergence.get_solution_times(sol_name)
+    channel1 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_000')
+    channel2 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_001')
+    channel3 = convergence.get_solution(sol_name, unit='unit_001', which='outlet_port_002')
+
+    plt.plot(time, channel1, label='channel 1')
+    plt.plot(time, channel2, label='channel 2')
+    plt.plot(time, channel3, label='channel 3')
+    plt.legend(fontsize=20)
+    plt.savefig(re.sub(".h5", ".png", sol_name), dpi=100, bbox_inches='tight')
+    
     model.save()
+    
