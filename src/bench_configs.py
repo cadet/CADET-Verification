179f1d81ae5e50c944d7edd6186629cb0e80a31b
Jan Breuer <j.m.br@gmx.de>
2025-05-21 16:15:26 +0200

    Fix radial flow benchmark names

--- src/bench_configs.py
+++ src/bench_configs.py
@@ -442,6 +442,10 @@ def radial_flow_benchmark(database_path, small_test=False, sensitivities=False):
             setting_radGRM_dynLin_1comp_benchmark1.get_model(database_path)
         ],
         'cadet_config_names': [
+            'radLRM_dynLin_1comp_sensbenchmark1',
+            'radLRMP_dynLin_1comp_sensbenchmark1',
+            'radGRM_dynLin_1comp_sensbenchmark1'
+        ] if sensitivities else [
             'radLRM_dynLin_1comp_benchmark1',
             'radLRMP_dynLin_1comp_benchmark1',
             'radGRM_dynLin_1comp_benchmark1'
