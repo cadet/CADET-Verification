c14a0df0cbb62724b8061fd5da343c9a181e82e2
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2025-07-23 14:01:12 +0200

    Add Pytest Configuration File

---
+++ conftest.py
@@ -0,0 +1,27 @@
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected (true/false).")
+
+def pytest_addoption(parser):
+    parser.addoption("--small-test", type=str2bool, default=True)
+    parser.addoption("--n-jobs", type=int, default=-1)
+    parser.addoption("--delete-h5-files", type=str2bool, default=True)
+
+    parser.addoption("--run-binding-tests", type=str2bool, default=False)
+    parser.addoption("--run-chromatography-tests", type=str2bool, default=True)
+    parser.addoption("--run-chromatography-sensitivity-tests", type=str2bool, default=False)
+    parser.addoption("--run-chromatography-system-tests", type=str2bool, default=False)
+    parser.addoption("--run-crystallization-tests", type=str2bool, default=False)
+    parser.addoption("--run-mct-tests", type=str2bool, default=False)
+    parser.addoption("--run-2dmodels-tests", type=str2bool, default=False)
+
+    parser.addoption("--commit-message", type=str, default="CADET model test run")
+    parser.addoption("--rdm-debug-mode", type=str2bool, default=True)
+    parser.addoption("--rdm-push", type=str2bool, default=False)
+    parser.addoption("--branch-name", type=str, default="main")
