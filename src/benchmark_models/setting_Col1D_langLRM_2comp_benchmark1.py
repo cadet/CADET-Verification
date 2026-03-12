568f12dca50ce12df91864c937a4182de240080e
MatthiasBur03 <mbur@smail.uni-koeln.de>
2026-03-12 02:14:32 +0100

    Add 2 component 1D Langmuir LRM benchmark setting

--- src/benchmark_models/setting_Col1D_langLRM_2comp_benchmark1.py
+++ src/benchmark_models/setting_Col1D_langLRM_2comp_benchmark1.py
@@ -10,36 +10,41 @@ from addict import Dict
 import numpy as np
 
 def get_model(
-        spatial_method_bulk,
+        spatial_method_bulk,        #tells CADET which method to use (FV or DG)
         refinement=1,
         **kwargs):
     
-    axNElem = 8 * kwargs.get('axRefinement', refinement)
-    parNElem = kwargs.get('parZ', 1)
+    axNElem = 8 * refinement                                   #number of axial discretization elements
     
     model = Dict()
     
-    model.input.model.nunits = 3
-    
-    # Flow sheet
-    model.input.model.connections.connections_include_ports = 0
+    model.input.model.nunits = 3                                #3 unit-operations (Inlet-Column-Outlet)
+
+    model.input.model.connections.connections_include_ports = 0 #dont use port numbers
     model.input.model.connections.nswitches = 1
-    model.input.model.connections.switch_000.connections = [
-        0.e+00, 1.e+00,-1.e+00,-1.e+00, 6.e-05, 1.e+00, 2.e+00,-1.e+00,-1.e+00, 6.e-05
-        ]
-    model.input.model.connections.switch_000.section = 0
-    
+
     #%% Column unit
     column = Dict()
     
-    column.UNIT_TYPE = 'COLUMN_MODEL_1D'
-    column.ncomp = 1
-    column.col_dispersion = 0.0001
+    column.UNIT_TYPE = 'LUMPED_RATE_MODEL_WITHOUT_PORES'
+    column.ncomp = 2
+    column.col_dispersion = kwargs.get("col_dispersion", 1e-05)
     column.col_length = 1.0
-    column.total_porosity = 0.6
-    column.velocity = 0.03333333333333333
-    column.npartype = 1
+    column.total_porosity = 0.4
+    # Flow sheet
+    #velocity = 0.1
+    A = column.col_length * 0.5
+    column.cross_section_area = A  
+
+    Q = 0.1 * A * column.total_porosity             # v = Q/(A*porosity)
+    model.input.model.connections.switch_000.connections = [
+        0.e+00, 1.e+00,-1.e+00,-1.e+00, Q, 
+        1.e+00, 2.e+00,-1.e+00,-1.e+00, Q
+        ]
+    model.input.model.connections.switch_000.section = 0
     
+    
+      
     if spatial_method_bulk > 0:
         column.discretization.SPATIAL_METHOD = "DG"
         column.discretization.EXACT_INTEGRATION = kwargs.get('exact_integration', 0)
@@ -57,20 +62,21 @@ def get_model(
         column.discretization.MAX_RESTARTS = 10
         column.discretization.SCHUR_SAFETY = 1.0e-8
     column.discretization.USE_ANALYTIC_JACOBIAN = 1
-    column.init_c = [ 0.0 ]
+    column.init_c = [ 0.0 , 0.0]
 
-    column.particle_type_000.has_film_diffusion = 0
-    column.particle_type_000.has_pore_diffusion = 0
-    column.particle_type_000.has_surface_diffusion = 0
-
-    column.particle_type_000.nbound = [ 1 ]
-    column.particle_type_000.adsorption.is_kinetic = kwargs.get('is_kinetic', 1)
-    column.particle_type_000.adsorption.lin_ka = [ 1.0 ]
-    column.particle_type_000.adsorption.lin_kd = [ 1.0 ]
-    column.particle_type_000.adsorption_model = 'LINEAR'
-    column.particle_type_000.init_cp = [ 0.0 ]
-    column.particle_type_000.init_cs = [ 0.0 ]
+    # Adsorption - LANGMUIR for 2 components
+    column.adsorption_model = 'MULTI_COMPONENT_LANGMUIR'
+    column.adsorption.is_kinetic = kwargs.get('is_kinetic', 0)
     
+    # Langmuir parameters for 2 components
+    column.adsorption.mcl_ka = kwargs.get('mcl_ka', [0.1, 0.05])
+    column.adsorption.mcl_kd = kwargs.get('mcl_kd', [1.0, 1.0])                 
+    column.adsorption.mcl_qmax = kwargs.get('mcl_qmax', [10.0, 10.0])
+    
+    column.nbound = [1, 1]           #comes from the binding model, specifies the number of bound states for each component
+    column.init_q = [0.0, 0.0]
+
+
     model.input.model.unit_001 = column
     
     #%% time integration parameters
@@ -83,30 +89,33 @@ def get_model(
     model.input.solver.consistent_init_mode = 1
     model.input.solver.consistent_init_mode_sens = 3
     model.input.solver.nthreads = 1
+
     model.input.solver.sections.nsec = 2
     model.input.solver.sections.section_continuity = [ 0 ]
-    model.input.solver.sections.section_times = [ 0.0, 10.0, 1500.0 ]
+    model.input.solver.sections.section_times = [ 0.0, 12.0, 40.0 ]
     model.input.solver.time_integrator.ABSTOL = kwargs.get('idas_reftol', 1e-12)
     model.input.solver.time_integrator.ALGTOL = kwargs.get('idas_reftol', 1e-10)
     model.input.solver.time_integrator.INIT_STEP_SIZE = 1e-10
     model.input.solver.time_integrator.MAX_STEPS = 10000
     model.input.solver.time_integrator.RELTOL = kwargs.get('idas_reftol', 1e-10)
-    model.input.solver.user_solution_times = np.linspace(0.0, 1500.0, 1500*4 + 1)
+    model.input.solver.user_solution_times = np.linspace(0.0, 40.0, 40*4 + 1)
     
     #%% auxiliary units: inlet and outlet
     model.input.model.unit_000.inlet_type = 'PIECEWISE_CUBIC_POLY'
-    model.input.model.unit_000.ncomp = 1
-    model.input.model.unit_000.sec_000.const_coeff = [1.]
-    model.input.model.unit_000.sec_000.cube_coeff = [0.]
-    model.input.model.unit_000.sec_000.lin_coeff = [0.]
-    model.input.model.unit_000.sec_000.quad_coeff = [0.]
-    model.input.model.unit_000.sec_001.const_coeff = [0.]
-    model.input.model.unit_000.sec_001.cube_coeff = [0.]
-    model.input.model.unit_000.sec_001.lin_coeff = [0.]
-    model.input.model.unit_000.sec_001.quad_coeff = [0.]
+    model.input.model.unit_000.ncomp = 2
+    #Feed concentration section 1
+    model.input.model.unit_000.sec_000.const_coeff = [10.0, 10.0]
+    model.input.model.unit_000.sec_000.cube_coeff = [0., 0.]
+    model.input.model.unit_000.sec_000.lin_coeff = [0., 0.]
+    model.input.model.unit_000.sec_000.quad_coeff = [0., 0.]
+    #Feed concentration section 2
+    model.input.model.unit_000.sec_001.const_coeff = [0.0, 0.0]
+    model.input.model.unit_000.sec_001.cube_coeff = [0., 0.]
+    model.input.model.unit_000.sec_001.lin_coeff = [0., 0.]
+    model.input.model.unit_000.sec_001.quad_coeff = [0., 0.]
     model.input.model.unit_000.UNIT_TYPE = 'INLET'
     
-    model.input.model.unit_002.ncomp = 1
+    model.input.model.unit_002.ncomp = 2
     model.input.model.unit_002.UNIT_TYPE = 'OUTLET'
     
     
@@ -130,49 +139,56 @@ def get_model(
     
     return model
 
+# === CADET self-check block ===
 
-def add_sensitivity_LRM_dynLin_1comp_benchmark1(model, sensName):
+import matplotlib.pyplot as plt
 
-    sensDepIdx = {
-        'COL_DISPERSION': {'sens_comp': np.int64(0)},
-        'TOTAL_POROSITY': { },
-        'LIN_KA': {'sens_comp': np.int64(0), 'sens_boundphase': np.int64(0)}
-    }
-
-    if sensName not in sensDepIdx:
-        raise Exception(f'Sensitivity dependencies for {sensName} unknown, please implement!')
-
-    if 'sensitivity' in model['input']:
-        model['input']['sensitivity']['NSENS'] += 1
-    else:
-        model['input']['sensitivity'] = {'NSENS': np.int64(1)}
-        model['input']['sensitivity']['sens_method'] = np.bytes_(b'ad1')
-
-    sensIdx = str(model['input']['sensitivity']['NSENS'] - 1).zfill(3)
+if __name__ == "__main__":
     
-    model['input']['sensitivity'][f'param_{sensIdx}'] = {}
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_name'] = str(sensName)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_unit'] = np.int64(1)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_partype'] = np.int64(-1)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_reaction'] = np.int64(-1)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_section'] = np.int64(-1)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_boundphase'] = np.int64(-1)
-    model['input']['sensitivity'][f'param_{sensIdx}']['sens_comp'] = np.int64(-1)
     
-    if sensName in sensDepIdx:
-        param = model['input']['sensitivity'][f'param_{sensIdx}']
-        for key, value in {**sensDepIdx[sensName]}.items():
-            model['input']['sensitivity'][f'param_{sensIdx}'][key] = value
-
-    return model
-
-
-def get_sensbenchmark1(spatial_method_bulk):
+    import os
+    import sys
     
-    model = get_model(spatial_method_bulk)
-    model['input'].pop('sensitivity', None)
-    model = add_sensitivity_LRM_dynLin_1comp_benchmark1(model, 'COL_DISPERSION')
-    model = add_sensitivity_LRM_dynLin_1comp_benchmark1(model, 'TOTAL_POROSITY')
-    model = add_sensitivity_LRM_dynLin_1comp_benchmark1(model, 'LIN_KA')
-    
-    return model
+    try:
+        from cadet import Cadet
+        model = Cadet()
+        model.root = get_model(spatial_method_bulk=0, refinement=8, col_dispersion=1e-5)   # 0=FV, 1=DG        
+        model.filename = "check_LRM_2comp.h5"
+        model.save()        
+        #Running CADET simulation
+        data = model.run()
+        
+                
+        # Try to display some results
+        try:
+            # Load the results from the model
+            model.load()
+            outlet_data = model.root.output.solution.unit_001.solution_outlet
+            sim_time = model.root.output.solution.solution_times
+            plt.plot(sim_time, outlet_data)
+            plt.show()
+
+
+        except Exception as e:
+            print(f"\n⚠ Could not extract sample data: {e}")
+            print("  (Model ran successfully, but couldn't read output)")
+        
+        print("\n" + "=" * 60)
+        print("=== Check complete ===")
+        print("=" * 60)
+        
+    except ImportError as e:
+        print(f"\n❌ ERROR: Could not import required module: {e}")
+        print("\nMake sure you have installed CADET-Python:")
+        print("  pip install cadet-python")
+        sys.exit(1)
+        
+    except Exception as e:
+        print(f"\n❌ ERROR: An unexpected error occurred:")
+        print(f"  {type(e).__name__}: {e}")
+        import traceback
+        print("\nFull traceback:")
+        print("-" * 60)
+        traceback.print_exc()
+        print("-" * 60)
+        sys.exit(1)
