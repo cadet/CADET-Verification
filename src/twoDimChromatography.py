6a157714459d6a3b6d34d0c0912145c134781387
Jan Breuer <j.m.br@gmx.de>
2026-05-04 19:29:16 +0200

    wip

--- src/twoDimChromatography.py
+++ src/twoDimChromatography.py
@@ -48,72 +48,72 @@ def get_settings(small_test):
             'film_diffusion': 0.0,
             # 'col_dispersion_radial' : 0.0,
             'nRadialZones': 2,
-            'name': '2DDPFR2Zone_1comp',
+            'name': '2DDPFR2Zone_1Comp',
             'par_method': 0,
             'adsorption_model': 'NONE',
             'surface_diffusion': 0.0,
-            'reference': load_reference('2DDPFR2Zone_1comp.h5')
+            'reference': load_reference('2DDPFR2Zone_1Comp.h5')
         },
-        # {  # 1parType, dynamic binding, no surface diffusion
-        #     'nRadialZones': 3,
-        #     'name': '2DGRM3Zone_dynLin_1Comp',
-        #     'par_method': 0,
-        #     'adsorption_model': 'LINEAR',
-        #     'adsorption.is_kinetic': 1,
-        #     'surface_diffusion': 0.0,
-        #     'reference': load_reference('2DGRM3Zone_dynLin_1Comp.h5')
-        # },
-        # {  # 1parType, dynamic binding, with surface diffusion
-        #     'nRadialZones': 3,
-        #     'name': '2DGRMsd3Zone_dynLin_1Comp',
-        #     'par_method': 0,
-        #     'adsorption_model': 'LINEAR',
-        #     'adsorption.is_kinetic': 1,
-        #     'surface_diffusion': 1e-11,
-        #     'reference': load_reference('2DGRMsd3Zone_dynLin_1Comp.h5')
-        # },
-        # {  # 1parType, req binding, no surface diffusion
-        #     'nRadialZones': 3,
-        #     'name': '2DGRM3Zone_reqLin_1Comp',
-        #     'par_method': 0,
-        #     'adsorption_model': 'LINEAR',
-        #     'adsorption.is_kinetic': 0,
-        #     'surface_diffusion': 0.0,
-        #     'init_cp': [0.0],
-        #     'init_cs': [0.0],
-        #     'reference': load_reference('2DGRM3Zone_reqLin_1Comp.h5')
-        # },
-        # {  # 1parType, req binding, with surface diffusion
-        #     'nRadialZones': 3,
-        #     'name': '2DGRMsd3Zone_reqLin_1Comp',
-        #     'par_method': 0,
-        #     'adsorption_model': 'LINEAR',
-        #     'adsorption.is_kinetic': 0,
-        #     'surface_diffusion': 1e-11,
-        #     'init_cp': [0.0],
-        #     'init_cs': [0.0],
-        #     'reference': load_reference('2DGRMsd3Zone_reqLin_1Comp.h5')
-        # },
-        # {  # 4parType:
-        #     'nRadialZones': 3,
-        #     'name': '2DGRM2parType3Zone_1Comp' if small_test else '2DGRM4parType3Zone_1Comp',
-        #     'par_method': 0,
-        #     'npartype': 2 if small_test else 4,
-        #     'par_type_volfrac': [0.5, 0.5] if small_test else [0.3, 0.35, 0.15, 0.2],
-        #     'par_radius': [45E-6, 75E-6] if small_test else [45E-6, 75E-6, 25E-6, 60E-6],
-        #     'par_porosity': [0.75, 0.7] if small_test else [0.75, 0.7, 0.8, 0.65],
-        #     'nbound': [1, 1] if small_test else [1, 1, 0, 1],
-        #     'init_cp': [0.0, 0.0] if small_test else [0.0, 0.0, 0.0, 0.0],
-        #     'init_cs': [0.0, 0.0] if small_test else [0.0, 0.0, 0.0, 0.0],
-        #     'film_diffusion': [6.9E-6, 6E-6] if small_test else [6.9E-6, 6E-6, 6.5E-6, 6.7E-6],
-        #     'pore_diffusion': [5E-11, 3E-11] if small_test else [6.07E-11, 5E-11, 3E-11, 4E-11],
-        #     'surface_diffusion': [5E-11, 0.0] if small_test else [1E-11, 5E-11, 0.0, 0.0],
-        #     'adsorption_model': ['LINEAR', 'LINEAR'] if small_test else ['LINEAR', 'LINEAR', 'NONE', 'LINEAR'],
-        #     'adsorption.is_kinetic': [0, 1] if small_test else [0, 1, 0, 0],
-        #     'adsorption.lin_ka': [35.5, 4.5] if small_test else [35.5, 4.5, 0, 0.25],
-        #     'adsorption.lin_kd': [1.0, 0.15] if small_test else [1.0, 0.15, 0, 1.0],
-        #     'reference': load_reference('2DGRM2parType3Zone_1Comp.h5' if small_test else '2DGRM4parType3Zone_1Comp.h5')
-        # }
+        {  # 1parType, dynamic binding, no surface diffusion
+            'nRadialZones': 3,
+            'name': '2DGRM3Zone_dynLin_1Comp',
+            'par_method': 0,
+            'adsorption_model': 'LINEAR',
+            'adsorption.is_kinetic': 1,
+            'surface_diffusion': 0.0,
+            'reference': load_reference('2DGRM3Zone_dynLin_1Comp.h5')
+        },
+        {  # 1parType, dynamic binding, with surface diffusion
+            'nRadialZones': 3,
+            'name': '2DGRMsd3Zone_dynLin_1Comp',
+            'par_method': 0,
+            'adsorption_model': 'LINEAR',
+            'adsorption.is_kinetic': 1,
+            'surface_diffusion': 1e-11,
+            'reference': load_reference('2DGRMsd3Zone_dynLin_1Comp.h5')
+        },
+        {  # 1parType, req binding, no surface diffusion
+            'nRadialZones': 3,
+            'name': '2DGRM3Zone_reqLin_1Comp',
+            'par_method': 0,
+            'adsorption_model': 'LINEAR',
+            'adsorption.is_kinetic': 0,
+            'surface_diffusion': 0.0,
+            'init_cp': [0.0],
+            'init_cs': [0.0],
+            'reference': load_reference('2DGRM3Zone_reqLin_1Comp.h5')
+        },
+        {  # 1parType, req binding, with surface diffusion
+            'nRadialZones': 3,
+            'name': '2DGRMsd3Zone_reqLin_1Comp',
+            'par_method': 0,
+            'adsorption_model': 'LINEAR',
+            'adsorption.is_kinetic': 0,
+            'surface_diffusion': 1e-11,
+            'init_cp': [0.0],
+            'init_cs': [0.0],
+            'reference': load_reference('2DGRMsd3Zone_reqLin_1Comp.h5')
+        },
+        {  # 4parType:
+            'nRadialZones': 3,
+            'name': '2DGRM2parType3Zone_1Comp' if small_test else '2DGRM4parType3Zone_1Comp',
+            'par_method': 0,
+            'npartype': 2 if small_test else 4,
+            'par_type_volfrac': [0.5, 0.5] if small_test else [0.3, 0.35, 0.15, 0.2],
+            'par_radius': [45E-6, 75E-6] if small_test else [45E-6, 75E-6, 25E-6, 60E-6],
+            'par_porosity': [0.75, 0.7] if small_test else [0.75, 0.7, 0.8, 0.65],
+            'nbound': [1, 1] if small_test else [1, 1, 0, 1],
+            'init_cp': [0.0, 0.0] if small_test else [0.0, 0.0, 0.0, 0.0],
+            'init_cs': [0.0, 0.0] if small_test else [0.0, 0.0, 0.0, 0.0],
+            'film_diffusion': [6.9E-6, 6E-6] if small_test else [6.9E-6, 6E-6, 6.5E-6, 6.7E-6],
+            'pore_diffusion': [5E-11, 3E-11] if small_test else [6.07E-11, 5E-11, 3E-11, 4E-11],
+            'surface_diffusion': [5E-11, 0.0] if small_test else [1E-11, 5E-11, 0.0, 0.0],
+            'adsorption_model': ['LINEAR', 'LINEAR'] if small_test else ['LINEAR', 'LINEAR', 'NONE', 'LINEAR'],
+            'adsorption.is_kinetic': [0, 1] if small_test else [0, 1, 0, 0],
+            'adsorption.lin_ka': [35.5, 4.5] if small_test else [35.5, 4.5, 0, 0.25],
+            'adsorption.lin_kd': [1.0, 0.15] if small_test else [1.0, 0.15, 0, 1.0],
+            'reference': load_reference('2DGRM2parType3Zone_1Comp.h5' if small_test else '2DGRM4parType3Zone_1Comp.h5')
+        }
     ]
 
 def GRM2D_linBnd_tests(
@@ -127,11 +127,11 @@ def GRM2D_linBnd_tests(
     
     ref_file_names = [
         '2DDPFR2Zone_1Comp.h5',
-        # '2DGRM3Zone_dynLin_1Comp.h5',
-        # '2DGRMsd3Zone_dynLin_1Comp.h5',
-        # '2DGRM3Zone_reqLin_1Comp.h5',
-        # '2DGRMsd3Zone_reqLin_1Comp.h5',
-        # '2DGRM2parType3Zone_1Comp.h5' if small_test else '2DGRM4parType3Zone_1Comp.h5'
+        '2DGRM3Zone_dynLin_1Comp.h5',
+        '2DGRMsd3Zone_dynLin_1Comp.h5',
+        '2DGRM3Zone_reqLin_1Comp.h5',
+        '2DGRMsd3Zone_reqLin_1Comp.h5',
+        '2DGRM2parType3Zone_1Comp.h5' if small_test else '2DGRM4parType3Zone_1Comp.h5'
         ]
 
 
@@ -207,57 +207,6 @@ def GRM2D_linBnd_tests(
 
         return benchmark_config
 
-    def GRM2D_DG_Benchmark(small_test=False, **kwargs):
-
-        nDisc = 3 if small_test else 4
-        nRadialZones = kwargs.get('nRadialZones', 3)
-
-        benchmark_config = {
-            'cadet_config_jsons': [
-                settings_2Dchromatography.GRM2D_linBnd_benchmark1(
-                    radNElem=nRadialZones,
-                    rad_inlet_profile=None,
-                    USE_MODIFIED_NEWTON=1, axMethod=3, **kwargs)
-            ],
-            'include_sens': [
-                False
-            ],
-            'ref_files': [
-                [kwargs.get('reference', None)]
-            ],
-            'refinement_ID': [
-                '000'
-            ],
-            'unit_IDs': [  # note that we consider radial zone 0
-                str(nRadialZones + 1 + 0).zfill(3)
-            ],
-            'which': [
-                'outlet' # outlet_port_000
-            ],
-            'idas_abstol': [
-                [1e-10]
-            ],
-            'ax_methods': [
-                [3]
-            ],
-            'ax_discs': [
-                [bench_func.disc_list(4, nDisc)]
-            ],
-            'rad_methods': [
-                [3]
-            ],
-            'rad_discs': [
-                [bench_func.disc_list(nRadialZones, nDisc)]
-            ],
-            'par_methods': [
-                [3]
-            ],
-            'par_discs': [
-                [bench_func.disc_list(max(1, nRadialZones - 1), nDisc)]
-            ]
-        }
-
-        return benchmark_config
 
     # %% create benchmark configurations
 
